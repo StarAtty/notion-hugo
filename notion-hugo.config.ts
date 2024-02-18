e8933b5f852dc0a9867b70a4d6e2b4bbbb841060
StarAtt <35491317+StarAtty@users.noreply.github.com>
2024-02-18 15:22:37 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://staratt.notion.site/Notion-DoIt-bd35f1a944354cc0b62dc0f4eae84b2c?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
